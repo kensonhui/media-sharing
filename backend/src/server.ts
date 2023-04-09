4a77ba7c38da17a8a8e069adcd4cb0e3d4d7906c
Kenson Hui <kenson.hui22@gmail.com>
2023-04-09 19:13:01 -0400

    Initial frontend and backend set up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,12 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 import s3Client from './services/s3client';
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import multer from "multer";
+import fs from "fs";
 
 dotenv.config();
 const app = express();
+const upload = multer({ dest: 'uploads/'});
 
 const EXPOSED_PORT = 5000;
 const corsOptions = {
@@ -15,7 +18,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
@@ -37,16 +41,17 @@ app.get("/push", async (req: Request, res: Response) => {
   }
 })
 
-app.post("/api/files", (req, res: Response) => {
+app.post("/api/files", upload.single("file"), (req, res: Response) => {
+  console.log(req.file);
   console.log(req.body);
-  s3Client.send(new PutObjectCommand({
-    Bucket: "files",
-    Key: req.body.name,
-    Body: req.body
-  }));
+  // s3Client.send(new PutObjectCommand({
+  //   Bucket: "files",
+  //   Key: name,
+  //   Body: file
+  // }));
   res.send({ data: req.body});
 })
 
 app.listen(EXPOSED_PORT, () => {
-  console.log(`Serve running on port ${EXPOSED_PORT}`);
+  console.log(`Server running on port ${EXPOSED_PORT}`);
 });
